cf54994c2e8e92db72133882c179e9e68626d148
21120430 <ngotuandat.lop9a@gmail.com>
2024-01-21 16:19:46 +0700

    add some settings to app.js

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+const hbs = require('express-handlebars');
+
+
+// set view engine 
+app.engine('.hbs', hbs.engine({
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+// set static files
+app.use(express.static('public'));
+
+// http
+app.listen((port) => {
+    console.log(`App listening on port ${port}`);
+})
