d84136a8eff9013356c1992d8b5465290a8ea871
PhamLene <phtmyle@gmail.com>
2024-01-23 19:54:16 +0700

    add products

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const accountRouter = require('./account.r');
+const adminRouter = require('./admin.r');
 
 function route(app) {
     app.use('/', accountRouter);
+    app.use('/', adminRouter);
 }
 
 module.exports = route;
