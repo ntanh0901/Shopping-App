3798ccc31f8ac784ae1460469ddd9921f27f2171
PhamLene <phtmyle@gmail.com>
2024-01-24 17:15:05 +0700

    overlay edit profile not work

--- public/js/edit_profile.js
+++ public/js/edit_profile.js
@@ -1,40 +1,52 @@
-function toggleInputVisibility(inputId, iconId) {
-    const inputElement = document.getElementById(inputId);
-    const iconElement = document.getElementById(iconId);
+$("#btn-edit").on("click", function () {
+  $(".dashboard-container, .edit-profile-container").toggle();
+});
 
-    if (inputElement.type === 'password') {
-        inputElement.type = 'text';
-        iconElement.classList.remove('fa-eye-slash');
-        iconElement.classList.add('fa-eye');
-    } else {
-        inputElement.type = 'password';
-        iconElement.classList.remove('fa-eye');
-        iconElement.classList.add('fa-eye-slash');
-    }
+function toggleInputVisibility(inputId, iconId) {
+  const inputElement = document.getElementById(inputId);
+  const iconElement = document.getElementById(iconId);
+
+  if (inputElement.type === "password") {
+    inputElement.type = "text";
+    iconElement.classList.remove("bi-eye-slash");
+    iconElement.classList.add("bi-eye");
+  } else {
+    inputElement.type = "password";
+    iconElement.classList.remove("bi-eye");
+    iconElement.classList.add("bi-eye-slash");
+  }
 }
 
-  function previewImage(event) {
-    const input = event.target;
-    const image = document.getElementById('avatar-image');
-    const largeImage = document.getElementById('largeImage');
+function previewImage(event) {
+  const input = event.target;
+  const image = document.getElementById("avatar-image");
+  const largeImage = document.getElementById("largeImage");
 
-    const file = input.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            image.src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-    }
+  const file = input.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      image.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
 
-    // Add click event to open the modal with the larger image
-    image.addEventListener('click', function () {
-        largeImage.src = image.src;
-        $('#imageModal').modal('show'); // Using jQuery to open Bootstrap modal
-    });
+  // Add click event to open the modal with the larger image
+  image.addEventListener("click", function () {
+    largeImage.src = image.src;
+    $("#imageModal").modal("show"); // Using jQuery to open Bootstrap modal
+  });
 }
 
 function openImageModal() {
-    // Function to open the modal when clicking on the profile image
-    $('#imageModal').modal('show');
+  // Function to open the modal when clicking on the profile image
+  $("#imageModal").modal("show");
 }
+
+$("#btn-cancel").on("click", function () {
+  $(".edit-profile-container, .dashboard-container").toggle();
+});
+
+$("#btn-confirm").on("click", function () {
+  $(".edit-profile-container, .dashboard-container").toggle();
+});
