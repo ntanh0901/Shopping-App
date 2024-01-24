f24226061b69e576c83d796f3f595c33e9472ebe
PhamLene <phtmyle@gmail.com>
2024-01-24 09:15:52 +0700

    fix admin dashboard

--- src/routes/admin.r.js
+++ src/routes/admin.r.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/admin', (req, res) => {
-    res.render('admin', {
-        title: 'admin'
-    })
+router.get("/admin", (req, res) => {
+  res.render("admin", {
+    title: "admin",
+  });
+});
+
+router.get("/admin/dashboard", (req, res) => {
+  res.render("dashboard", {
+    title: "Admin dashboard page",
+    // isAdmin: true,
+    isDashboard: true,
+  });
 });
 module.exports = router;
