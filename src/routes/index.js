db0ad1f1f9ab772c30f1f5bfa24edd78452925f1
21120430 <ngotuandat.lop9a@gmail.com>
2024-01-24 16:24:42 +0700

    add home router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const accountRouter = require('./account.r');
+const homeRouter = require('./home.r');
 
 function route(app) {
     app.use('/', accountRouter);
+    app.use('/', homeRouter);
 }
 
 module.exports = route;
