d18bf855dc1c5d2af184dac830f3136718368781
PhamLene <phtmyle@gmail.com>
2024-01-27 00:26:36 +0700

    edit profile admin, not validate

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,21 +1,52 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var tooltips = document.querySelectorAll('[data-toggle="tooltip"]');
-    tooltips.forEach(function (tooltip) {
-        new bootstrap.Tooltip(tooltip);
-    });
-});
-const rows = document.querySelectorAll("tbody tr"); rows.forEach(function
-    (row) {
-    const statusCell = row.querySelector("td:nth-child(4)"); const status
-        = statusCell.textContent.trim(); switch (status) {
-            case "Đã thanh toán":
-                statusCell.classList.toggle("paid"); break; case "Chưa thanh toán":
-                statusCell.classList.toggle("pending"); break; case "Hủy":
-                statusCell.classList.toggle("canceled"); break;
-        }
-});
+let adminInfo = {
+  image: "/img/logo_hcmus.png",
+  username: "Admin",
+  name: "Nguyễn Admin",
+  age: 24,
+  sex: "nam",
+  phone: "0867564837",
+  email: "admin@example.com",
+  address: "Admin address",
+};
 
-$("#btn-edit").on("click", function () {
-    $(".dashboard-container, .edit-profile-container").toggle();	
-	$(".edit-profile-container").toggleClass("d-flex");
-});
+updateDashboard();
+
+function updateDashboard(){
+	$("#adminImage").text(adminInfo.image);
+	$("#adminUsername").text(adminInfo.username);
+	$("#adminName").text(adminInfo.name);
+	$("#adminAge").text(adminInfo.age);
+	$("#adminSex").text(adminInfo.sex);
+	$("#adminPhone").text(adminInfo.phone);
+	$("#adminEmail").text(adminInfo.email);
+	$("#adminAddress").text(adminInfo.address);
+}
+
+$("#editProfileModal").on("show.bs.modal", function (event) {
+	populateEditProfileModal();
+  });
+
+function populateEditProfileModal() {
+  $("#editImage").val(adminInfo.image);
+  $("#editUsername").val(adminInfo.username);
+  $("#editName").val(adminInfo.name);
+  $("#editAge").val(adminInfo.age);
+  $("#editNam").prop("checked",adminInfo.sex.toLowerCase() === "nam" ? true : false);
+  $("#editNu").prop("checked", adminInfo.sex.toLowerCase() === "nữ" ? true : false);
+  $("#editPhone").val(adminInfo.phone);
+  $("#editEmail").val(adminInfo.email);
+  $("#editAddress").val(adminInfo.address);
+}
+function submitForm() {
+  adminInfo.username = $("#editUsername").val();
+  adminInfo.name = $("#editName").val();
+  adminInfo.age = $("#editAge").val();
+  adminInfo.sex = $('input[name="editGender"]:checked').val();
+  adminInfo.phone = $("#editPhone").val();
+  adminInfo.email = $("#editEmail").val();
+  adminInfo.address = $("#editAddress").val();
+
+  updateDashboard();
+  $("#editProfileModal").modal("hide");
+}
+
